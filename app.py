1c9578847bacd9da8d72747bec98ec5268b9267d
Willy Aguirre <marti1125@gmail.com>
2017-10-02 11:49:34 -0500

    webhooks para la creacionde token y cargo api v1.2

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import request
 import logging
 from logging.handlers import RotatingFileHandler
 import culqipy
+import culqipy1_2
+import uuid
+
+from pip import req
 
 app = Flask(__name__)
 
@@ -12,6 +16,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/1_2', methods=['GET'])
+def index1_2():
+    return render_template('index1_2.html')
 
 
 @app.route('/token', methods=['GET'])
@@ -46,6 +55,39 @@ def charge():
     return jsonify({'error': 'nopost'})
 
 
+@app.route('/cargos', methods=['POST'])
+def cargos():
+    if request.method == 'POST':
+
+        token = request.form['token']
+
+        culqipy1_2.llave_privada = '4QEdt+TTC1vSMga29rdHMz+h2564jFKKl50XWxOFskk='
+
+        cargo = {
+                    'token': token,
+                    'moneda': 'PEN',
+                    'monto': 200,
+                    'descripcion': 'Venta de prueba',
+                    'pedido': str(uuid.uuid4())[:8],
+                    'codigo_pais': 'PE',
+                    'direccion': 'Avenida Lima 123',
+                    'ciudad': 'Lima',
+                    'usuario': 'marti1125',
+                    'telefono': 3333339,
+                    'nombres': 'Willy',
+                    'apellidos': 'Aguirre',
+                    'correo_electronico': 'marti1125@gmail.com'
+                 }
+
+        cargo_respuesta = culqipy1_2.Cargo.crear(cargo)
+
+        app.logger.info('respuesta creacion de cargo >>> %s', cargo_respuesta)
+
+        return jsonify(cargo_respuesta)
+
+    return jsonify({'error': 'nopost'})
+
+
 @app.route('/tokens', methods=['POST'])
 def tokens():
     if request.method == 'POST':
@@ -59,35 +101,52 @@ def tokens():
     return jsonify({'error': 'nopost'})
 
 
+# WEBHOOKS PARA CULQI API V1.2
+
+# TOKEN
+@app.route('/webhook_1_2/token/creacion', methods=['POST'])
+def creacion_token():
+    app.logger.info('response webhook - creacion de token >>> %s', request.data)
+    return jsonify({'response': 'creacion de token'})
+
+# CARGO
+@app.route('/webhook_1_2/cargo/exitoso', methods=['POST'])
+def cargo_exitoso():
+    app.logger.info('response webhook - cargo exitoso >>> %s', request.data)
+    return jsonify({'response': 'cargo exitoso'})
+
+
+# WEBHOOKS PARA CULQI API V2
+
 # TOKEN
 @app.route('/webhook/token/creation/succeeded', methods=['POST'])
 def token_creation_succeeded():
-    app.logger.info('response webhook - token creation succeeded >>> %s', charge)
+    app.logger.info('response webhook - token creation succeeded >>> %s', request.data)
     return jsonify({'response': 'token creation succeeded'})
 
 
 @app.route('/webhook/token/expired', methods=['POST'])
 def token_expired():
-    app.logger.info('response webhook - token expired >>> %s', charge)
+    app.logger.info('response webhook - token expired >>> %s', request.data)
     return jsonify({'response': 'token expired'})
 
 
 @app.route('/webhook/token/creation/failed', methods=['POST'])
 def token_creation_failed():
-    app.logger.info('response webhook - token creation failed >>> %s', charge)
+    app.logger.info('response webhook - token creation failed >>> %s', request.data)
     return jsonify({'response': 'token creation failed'})
 
 
 #CHARGE
 @app.route('/webhook/charge/creation/succeeded', methods=['POST'])
 def charge_creation_succeeded():
-    app.logger.info('response webhook - charge creation succeeded >>> %s', charge)
+    app.logger.info('response webhook - charge creation succeeded >>> %s', request.data)
     return jsonify({'response': 'charge creation succeeded'})
 
 
 @app.route('/webhook/charge/creation/failed', methods=['POST'])
 def charge_creation_failed():
-    app.logger.info('response webhook - charge creation failed >>> %s', charge)
+    app.logger.info('response webhook - charge creation failed >>> %s', request.data)
     return jsonify({'response': 'charge creation failed'})
 
 
